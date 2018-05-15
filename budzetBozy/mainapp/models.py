3c2b165c26c55e10011fcf55cf6b7ac09fd35721
Gures97 <19gures97@gmail.com>
2018-05-15 11:38:47 +0200

    Powklejano modele i settings

--- budzetBozy/mainapp/models.py
+++ budzetBozy/mainapp/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from datetime import date
 
-# Create your models here.
+class User(models.Model):
+    first_name = models.CharField('imię',max_length=100)
+    last_name = models.CharField('nazwisko',max_length=100)
+    type = models.CharField(max_length=100)
+
+class Account(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    balance = models.DecimalField('saldo',max_digits=100,decimal_places=2)
+
+class Currency(models.Model):
+    id = models.CharField(max_length=3,primary_key=True)
+    rate = models.DecimalField('kurs',max_digits=5,decimal_places=4)
+
+class Category(models.Model):
+    description = models.CharField('nazwa',max_length=100)
+
+class Transactions(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    account = models.ForeignKey(Account, on_delete = models.CASCADE)
+    date = models.DateField('data',default=date.today)
+    amount = models.DecimalField('kwota',max_digits=100,decimal_places=2)
+    currency = models.ForeignKey(Currency,on_delete = models.CASCADE,verbose_name="waluta")
+    category = models.ForeignKey(Category,on_delete = models.CASCADE, verbose_name="kategoria")
+

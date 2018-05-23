fff129931d0f63dcdbb75af05a2c9a8b2781e93e
Gures97 <19gures97@gmail.com>
2018-05-23 19:20:06 +0200

    Dodano base.html i pare innych rzeczy

--- budzetBozy/budzetBozy/urls.py
+++ budzetBozy/budzetBozy/urls.py
@@ -18,6 +18,6 @@ from django.urls import path
 from django.conf.urls import include, url
 
 urlpatterns = [
-    url(r'^mainapp/', include('mainapp.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^', include('mainapp.urls')),
 ]
--- budzetBozy/mainapp/views.py
+++ budzetBozy/mainapp/views.py
@@ -5,3 +5,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('Oto i widok, elo xD')
+
+def podstrona(request,param1,param2):
+    response = '<b>Parametr 1:</b> ' + param1 + '<br>' + '<b>Parametr 2:</b> ' + param2
+    return HttpResponse(response)

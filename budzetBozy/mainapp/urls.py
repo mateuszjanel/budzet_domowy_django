7feac76246b3624168b3a51382a72cf0360de6df
Gures97 <19gures97@gmail.com>
2018-05-29 11:33:22 +0200

    Napsute troche ale dwie nowe templatełki

--- budzetBozy/mainapp/urls.py
+++ budzetBozy/mainapp/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^raport$', views.raport, name='raport'),
     url(r'^(?P<param2>[a-z]*)/(?P<param1>[a-z]*)$', views.podstrona, name='podstrona')
 ]
--- budzetBozy/mainapp/views.py
+++ budzetBozy/mainapp/views.py
@@ -2,10 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import Transaction
 
 def index(request):
-    return render(request,'base.html')
+    return render(request,'index.html')
 
 def podstrona(request,param1,param2):
     response = '<b>Parametr 1:</b> ' + param1 + '<br>' + '<b>Parametr 2:</b> ' + param2
     return HttpResponse(response)
+
+def raport(request):
+    transactions = Transaction.objects
+    context = {'transactions' : transactions}
+    return render(request,'raport.html', context)

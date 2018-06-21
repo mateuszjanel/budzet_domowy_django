3d532b6332566267913a2db66653ed3aec5a9885
Natalia <mcb.nat@gmail.com>
2018-06-21 06:58:47 +0200

    Usuwanie kategorii, wybór rodzaju transakcji, ZMIANY W MODELACH

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     url(r'^dodanie_transakcji$', views.dodanie_transakcji,name='dodanie_transakcji'),
     path('usuwanie_transakcji/<int:id>', views.usuwanie_transakcji,name='usuwanie_transakcji'),
     url(r'^dodanie_kategorii$', views.dodanie_kategorii, name='dodanie_kategorii'),
+    path('usuwanie_kategorii/<int:id>',views.usuwanie_kategorii, name='usuwanie_kategorii'),
     url(r'^dodanie_konta$', views.dodanie_konta, name='dodanie_konta'),
     path('usuwanie_konta/<int:id>', views.usuwanie_konta,name='usuwanie_konta'),
     url(r'^dodanie_zlecenia_stalego$', views.dodanie_zlecenia_stalego, name='dodanie_zlecenia_stalego'),
